26615b65a3c1157ef9c318ed1772a59f337f3a69
kangfadho <152151825+kangfadho@users.noreply.github.com>
2026-07-09 23:03:26 +0700

    tak tambahi flileku

---
+++ fadho.js
@@ -0,0 +1,15 @@
+let username= 'muhammad muhafadhoh'
+let password= 'fadhoGanteng123'
+let umur= 12;
+
+if (username == 'muhammad muhafadhoh' && password=='fadhoGanteng123'){
+    if (umur >=18){
+        console.log(`Hai ${username}`);
+        console.log('Selamat Datang di Situs Gelapp');
+    } else {
+        console.log(`Umur Anda tidak Mencukupi ${username}`);
+        console.log('Sebaiknya Anda keluar');
+    }
+} else {
+    console.log('Tidak Bisa Login');
+}
