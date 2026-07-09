d357c31200c0d7eb4f607bb1e82a4b8e2ff12988
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-07-09 23:13:00 +0700

    feat: menambah fitur looping

--- fadho.js
+++ fadho.js
@@ -1,15 +1,21 @@
-let username= 'muhammad muhafadhoh'
-let password= 'fadhoGanteng123'
-let umur= 12;
+let username = "muhammad muhafadhoh";
+let password = "fadhoGanteng123";
+let umur = 12;
 
-if (username == 'muhammad muhafadhoh' && password=='fadhoGanteng123'){
-    if (umur >=18){
-        console.log(`Hai ${username}`);
-        console.log('Selamat Datang di Situs Gelapp');
-    } else {
-        console.log(`Umur Anda tidak Mencukupi ${username}`);
-        console.log('Sebaiknya Anda keluar');
-    }
+if (username == "muhammad muhafadhoh" && password == "fadhoGanteng123") {
+  if (umur >= 18) {
+    console.log(`Hai ${username}`);
+    console.log("Selamat Datang di Situs Gelapp");
+  } else {
+    console.log(`Umur Anda tidak Mencukupi ${username}`);
+    console.log("Sebaiknya Anda keluar");
+  }
 } else {
-    console.log('Tidak Bisa Login');
+  console.log("Tidak Bisa Login");
+}
+
+let angka = 10;
+
+for (let index = 0; index < angka; index++) {
+  console.log(index);
 }
